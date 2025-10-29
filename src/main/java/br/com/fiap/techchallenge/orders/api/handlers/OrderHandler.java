f2848d298cb9a9c8688d39ab68819ae60c62b5b0
Wesley Alves <wesley.alves@mtn.com.br>
2025-10-29 20:30:01 -0300

    chore: hello world! starts project.

---
+++ src/main/java/br/com/fiap/techchallenge/orders/api/handlers/OrderHandler.java
@@ -0,0 +1,17 @@
+package br.com.fiap.techchallenge.orders.api.handlers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderHandler {
+    public OrderHandler() {}
+
+    @GetMapping("/")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
