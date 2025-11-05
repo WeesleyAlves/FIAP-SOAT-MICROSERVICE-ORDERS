33478a55aedd10d3efd2d4cac2ce4b2a36a719ce
Wesley Alves <wesley.alves@mtn.com.br>
2025-11-05 19:44:04 -0300

    chore: initial commit into main

---
+++ src/main/java/br/com/fiap/techchallenge/orders/FastfoodOrdersApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenge.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodOrdersApplication.class, args);
+	}
+
+}
